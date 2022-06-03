a337ca4585fd03fcb6f466010ce5700652db7f2b
Mehti <102772767+Mehtisup@users.noreply.github.com>
2022-06-03 23:15:05 +0430

    Update and rename Project 2 to welcome.php
    PHP collect data

---
+++ welcome.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>PHP</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+
+</style>
+</head>
+<body>
+
+<form action="game.php" method="post">
+    <label for="user">Username</label><br>
+    <input type="text" name="user"><br>
+    <label for="age">Age</label><br>
+    <input type="text" name="age"><br>
+    <br>
+    <button type="submit">Submit</button>
+</form>
+
+</body>
+</html>
